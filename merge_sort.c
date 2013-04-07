5814bfac58ea6ac2db0ff8192b050674a6e9143e
sswly <sswlymail@gmail.com>
2013-04-07 17:56:35 +0800

    improve for merge sort

--- merge_sort.c
+++ merge_sort.c
@@ -1,7 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
 #include "utils.c"
+void que_cpy(int dst_que[], int src_que[], int len)
+{
+    int i = 0;
+    for (; i < len; i++)
+    {
+        dst_que[i] = src_que[i];
+    }
+}
 void merge(int que[], int p, int q, int r)
 {
+    int id = 0;
+    int *tmp_que = malloc((r - p) * sizeof(int));
+    int q1 = q + 1;
+    int p1 = p;
+    while (p1 <= q || q1 <= r)
+    {
+        if (que[p1] > que[q1])
+        {
+            tmp_que[id] = que[q1];
+            q1++;
+        }
+        else
+        {
+            tmp_que[id] = que[p1];
+            p1++;
+        }
+        id++;
+    }
 
+    que_cpy(&que[p], tmp_que, r - p);
+    free(tmp_que);
 }
 
 void merge_sort(int que[], int p, int r)
@@ -18,10 +48,11 @@ void merge_sort(int que[], int p, int r)
 int main()
 {
     //int que[10] = {10,7,8,6,4,3,2,9,5,1};
-    int que[10] = {10,9,8,7,6,5,4,3,2,1};
+    //int que[10] = {10,9,8,7,6,5,4,3,2,1};
     //int que[10] = {1,2,3,4,5,6,7,8,9,10};
-    merge_sort(que, 10);
-    dump_que(que, 10);
+    int que[2] = {1,0};
+    merge_sort(que, 0, 1);
+    dump_que(que, 2);
     printf("Times is O(%u) = %u\n", 10, O);
     return 0;
 }
